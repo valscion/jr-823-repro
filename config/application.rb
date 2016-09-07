9b7468c386963796b75ce6f49bb99ea2d631c72d
Vesa Laakso <vesa.laakso@venuu.fi>
2016-09-07 14:54:44 +0300

    Add the dangerous config that triggers the crash :grimacing:

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Jr823Repro
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Load models from subfolders
+    config.eager_load_paths += Dir["#{config.root}/app/models/**/"]
   end
 end
